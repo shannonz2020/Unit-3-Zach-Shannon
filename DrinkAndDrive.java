d5d06754c6f364b613ddc923eb7828a538287688
Zach Shannon <shannonz2020@gmail.com>
2019-10-03 16:47:38 -0700

    Added DrinkAndDrive

--- DrinkAndDrive.java
+++ DrinkAndDrive.java
@@ -4,5 +4,13 @@ import java.util.Scanner;
       Scanner ageInput = new Scanner(System.in);
       System.out.println("Enter your age: ");
       int userAge = ageInput.nextInt();
+      //Print statement
+      if(userAge<17){
+        System.out.println("You're not old enough to drive or drink!");
+      }else if(userAge>=17 && userAge<21){
+        System.out.println("Hey! You can drive, just no drinking, idiot!");
+      }else{
+        System.out.println("Woah! You're really old. Have fun drinking and driving!");
+      }
     }
   }
