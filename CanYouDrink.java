f3f9c30892fe723a5ff4025a581636aaa27e6754
Zach Shannon <shannonz2020@gmail.com>
2019-10-03 09:23:32 -0700

    CanYouDrink has been added

---
+++ CanYouDrink.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+  public class CanYouDrink{
+    public static void main(String[] args){
+      //Read user input
+      Scanner ageInput = new Scanner(System.in);
+  }
+}
