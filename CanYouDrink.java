d0d21de1567027dad374e8597366e896a749503d
Zach Shannon <shannonz2020@gmail.com>
2019-10-03 09:54:22 -0700

    Added DrinkAndDrive

--- CanYouDrink.java
+++ CanYouDrink.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
     public static void main(String[] args){
       //Read user input
       Scanner ageInput = new Scanner(System.in);
+      System.out.println("Enter your age: ");
+      int userAge = ageInput.nextInt();
+      //Print statement
+      if(userAge >= 21){
+        System.out.println("Woo-hoo! You can drink!");
+      } else{
+        System.out.println("You're too young to drink, grow up!");
+      }
   }
 }
---
+++ DrinkAndDrive.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+  public class DrinkAndDrive{
+    public static void main(String[] args){
+      Scanner ageInput = new Scanner(System.in);
+      System.out.println("Enter your age: ");
+      int userAge = ageInput.nextInt();
+    }
+  }
