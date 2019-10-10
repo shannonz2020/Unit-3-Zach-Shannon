70b024d11fc5bdaf3bc0322a17fd14ff686b31d9
Zach Shannon <shannonz2020@gmail.com>
2019-10-10 09:56:05 -0700

    Added PalindromeTester and StringComparison

---
+++ PalindromeTester.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PalindromeTester{
+  public static void main(String[] args){
+    Scanner userString = new Scanner(System.in);
+    System.out.println("Enter a palindrome: ");
+    //test with "Kayak"
+    String palindromeOriginal = userString.next();
+    for(int i = 0; i != palindrome.length; i++){
+      char = 
+      String palindromeNew = "" + char +palindromeNew;
+    }
+  }
+}
---
+++ StringComparisons.java
@@ -0,0 +1,7 @@
+public class StringComparisons{
+  public static void main(String[] args){
+    String string1 = "its";
+    String string2 = "it's";
+    System.out.println(string1.compareTo(string2));
+  }
+}
